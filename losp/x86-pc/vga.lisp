973c2ac34b349c98103300abab3bcd9baff21d44
ffjeld <ffjeld>
2004-04-16 19:17:55 +0000

    Added some VGA register accessors.

--- losp/x86-pc/vga.lisp
+++ losp/x86-pc/vga.lisp
@@ -10,7 +10,7 @@
 ;;;; Author:        Frode Vatvedt Fjeld <frodef@acm.org>
 ;;;; Created at:    Tue Sep 25 14:08:20 2001
 ;;;;                
-;;;; $Id: vga.lisp,v 1.3 2004/01/19 11:23:52 ffjeld Exp $
+;;;; $Id: vga.lisp,v 1.4 2004/04/16 19:17:55 ffjeld Exp $
 ;;;;                
 ;;;;------------------------------------------------------------------
 
@@ -19,17 +19,17 @@
 
 (in-package muerte.x86-pc)
 
-(defun (setf vga-crt-controller-register) (value register)
-  (let* ((address-register (if (logbitp 0 (io-port #x3cc :unsigned-byte8)) #x3d4 #x3b4))
-	 (data-register (1+ address-register)))
-    (setf (io-port address-register :unsigned-byte8) register
-	  (io-port data-register :unsigned-byte8) value)))
-
 (defun vga-crt-controller-register (register)
   (let* ((address-register (if (logbitp 0 (io-port #x3cc :unsigned-byte8)) #x3d4 #x3b4))
 	 (data-register (1+ address-register)))
     (setf (io-port address-register :unsigned-byte8) register)
     (io-port data-register :unsigned-byte8)))
+
+(defun (setf vga-crt-controller-register) (value register)
+  (let* ((address-register (if (logbitp 0 (io-port #x3cc :unsigned-byte8)) #x3d4 #x3b4))
+	 (data-register (1+ address-register)))
+    (setf (io-port address-register :unsigned-byte8) register
+	  (io-port data-register :unsigned-byte8) value)))
 
 (defun vga-graphics-register (register)
   (setf (io-port #x3ce :unsigned-byte8) register)
@@ -60,3 +60,17 @@
     (#b01 (values #xa0000 #xaffff))
     (#b10 (values #xb0000 #xb7fff))
     (#b11 (values #xb8000 #xbffff))))
+
+(defun vga-horizontal-display-end ()
+  (1+ (vga-crt-controller-register 1)))
+
+(defun vga-vertical-display-end ()
+  (let ((overflow (vga-crt-controller-register 7)))
+    (+ 1
+       (vga-crt-controller-register #x12)
+       (if (logbitp 1 overflow) #x100 0)
+       (if (logbitp 6 overflow) #x200 0))))
+
+(defun vga-character-height ()
+  (1+ (ldb (byte 5 0)
+	   (vga-crt-controller-register 9))))
