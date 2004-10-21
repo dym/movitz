6a10934a838c27d892dff69b80dd924352d0f21f
ffjeld <ffjeld>
2004-10-21 20:40:32 +0000

    Removed dead code.

--- image.lisp
+++ image.lisp
@@ -9,7 +9,7 @@
 ;;;; Created at:    Sun Oct 22 00:22:43 2000
 ;;;; Distribution:  See the accompanying file COPYING.
 ;;;;                
-;;;; $Id: image.lisp,v 1.71 2004/09/25 15:40:30 ffjeld Exp $
+;;;; $Id: image.lisp,v 1.72 2004/10/21 20:40:32 ffjeld Exp $
 ;;;;                
 ;;;;------------------------------------------------------------------
 
@@ -1457,21 +1457,7 @@ this image will not be Multiboot compatible."
 	     (make-movitz-hash-table expr))
 	    (ratio
 	     (make-instance 'movitz-ratio
-	       :value expr)
-	     #+ignore
-	     (let ((slot-descriptions (gethash 'muerte.cl::ratio
-					       (image-struct-slot-descriptions *image*)
-					       nil)))
-	       (unless slot-descriptions
-		 (error "Don't know how to movitz-read ratios (yet)." expr))
-	       (let ((movitz-object (make-instance 'movitz-struct
-				      :class (muerte::movitz-find-class 'muerte.cl::ratio)
-				      :length (length slot-descriptions))))
-		 (setf (image-lisp-to-movitz-object *image* expr) movitz-object)
-		 (setf (slot-value movitz-object 'slot-values)
-		   (list (movitz-read (numerator expr))
-			 (movitz-read (denominator expr))))
-		 movitz-object)))
+	       :value expr))
 	    (structure-object
 	     (let ((slot-descriptions (gethash (type-of expr)
 					       (image-struct-slot-descriptions *image*)
