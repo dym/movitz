04e8b22a8ba8293cf70364f22aa4924a61834196
ffjeld <ffjeld>
2005-04-18 07:08:42 +0000

    *** empty log message ***

--- losp/los0.lisp
+++ losp/los0.lisp
@@ -9,7 +9,7 @@
 ;;;; Created at:    Fri Dec  1 18:08:32 2000
 ;;;; Distribution:  See the accompanying file COPYING.
 ;;;;                
-;;;; $Id: los0.lisp,v 1.38 2005/04/15 07:04:24 ffjeld Exp $
+;;;; $Id: los0.lisp,v 1.39 2005/04/18 07:08:42 ffjeld Exp $
 ;;;;                
 ;;;;------------------------------------------------------------------
 
@@ -1330,6 +1330,8 @@ Can be used to measure the overhead of primitive function."
     (:movl 4 :eax)
    no-overflow))
 
+(defvar *segment-descriptor-table*)
+
 (defun genesis ()
   ;; (install-shallow-binding)
   (let ((extended-memsize 0))
@@ -1341,6 +1343,11 @@ Can be used to measure the overhead of primitive function."
     (format t "Extended memory: ~D KB~%" extended-memsize)
 
     (idt-init)
+    
+    (setf *segment-descriptor-table*	; Ensure we have a GDT with 16 entries, in static-space.
+      (muerte::install-global-segment-table
+       (muerte::dump-global-segment-table :entries 16)))
+    
     (install-los0-consing :kb-size 500)
     #+ignore
     (install-los0-consing :kb-size (max 50 (truncate (- extended-memsize 2048) 2))))
