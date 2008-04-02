a303f139832cf3cfd5d4e8a6225973199ffa0d97
ffjeld <ffjeld>
2008-04-02 20:47:09 +0000

    Set an appropriate value (i.e. scannable) for the :run-time-context tag.

--- storage-types.lisp
+++ storage-types.lisp
@@ -9,7 +9,7 @@
 ;;;; Created at:    Sun Oct 22 00:22:43 2000
 ;;;; Distribution:  See the accompanying file COPYING.
 ;;;;                
-;;;; $Id: storage-types.lisp,v 1.62 2008-03-23 12:19:19 ffjeld Exp $
+;;;; $Id: storage-types.lisp,v 1.63 2008-04-02 20:47:09 ffjeld Exp $
 ;;;;                
 ;;;;------------------------------------------------------------------
 
@@ -68,7 +68,8 @@
   :null 5
   :other 6
   :symbol 7
-  
+
+  ;; The lower 3 bits of these are significant in mysterious ways.
   :basic-vector #x22
   :defstruct #x2a
   :funobj #x3a
@@ -76,7 +77,7 @@
   :ratio #x52
   :complex #x5a
   :std-instance #x40
-  :run-time-context #x50
+  :run-time-context #x62
   :illegal #x13
   :infant-object #x23
   :basic-restart #x32
@@ -342,9 +343,10 @@ integer (native lisp) value."
 		   :u8 2
 		   :u16 3
 		   :u32 4
-		   :bit 5
-		   :code 6
-		   :indirects 7)
+		   :stack 5
+		   :bit 6
+		   :code 7
+		   :indirects 8)
     :initarg :element-type
     :reader movitz-vector-element-type)
    (fill-pointer
@@ -396,7 +398,7 @@ integer (native lisp) value."
 
 (defun movitz-vector-element-type-size (element-type)
   (ecase element-type
-    ((:any-t :u32) 32)
+    ((:any-t :u32 :stack) 32)
     ((:character :u8 :code) 8)
     (:u16 16)
     (:bit 1)))
