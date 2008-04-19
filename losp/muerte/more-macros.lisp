8a32fca1860dc8bac1b373634e421989ca592c23
ffjeld <ffjeld>
2008-04-19 12:44:40 +0000

    Add a run-time define-compiler-macro placeholder.

--- losp/muerte/more-macros.lisp
+++ losp/muerte/more-macros.lisp
@@ -10,7 +10,7 @@
 ;;;; Author:        Frode Vatvedt Fjeld <frodef@acm.org>
 ;;;; Created at:    Fri Jun  7 15:05:57 2002
 ;;;;                
-;;;; $Id: more-macros.lisp,v 1.44 2008-04-17 19:35:05 ffjeld Exp $
+;;;; $Id: more-macros.lisp,v 1.45 2008-04-19 12:44:40 ffjeld Exp $
 ;;;;                
 ;;;;------------------------------------------------------------------
 
@@ -575,3 +575,9 @@ respect to multiple threads."
 	 (*read-suppress* nil)
 	 #+ignore (*readtable* nil))
      ,@body))
+
+(defmacro/run-time define-compiler-macro (name &rest ignore)
+  (declare (ignore ignore))
+  'compiler-macro
+  name)
+  
