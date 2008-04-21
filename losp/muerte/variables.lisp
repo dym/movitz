0d87f0adff5ef9c9a59af109007d7333d08be3ce
ffjeld <ffjeld>
2008-04-21 19:44:13 +0000

    Add misc. standard variables.

--- losp/muerte/variables.lisp
+++ losp/muerte/variables.lisp
@@ -10,7 +10,7 @@
 ;;;; Author:        Frode Vatvedt Fjeld <frodef@acm.org>
 ;;;; Created at:    Wed Nov  5 21:53:34 2003
 ;;;;                
-;;;; $Id: variables.lisp,v 1.12 2008-03-21 22:30:40 ffjeld Exp $
+;;;; $Id: variables.lisp,v 1.13 2008-04-21 19:44:13 ffjeld Exp $
 ;;;;                
 ;;;;------------------------------------------------------------------
 
@@ -35,6 +35,7 @@
 (defvar +++ nil)
 
 (defvar *read-base* 10)
+(defvar *read-eval* t)
 (defvar *package* nil)
 
 (defvar *macroexpand-hook* 'funcall)
@@ -63,11 +64,38 @@
 (defvar single-float-negative-epsilon -1/1000)
 (defvar double-float-negative-epsilon -1/1000)
 (defvar long-float-negative-epsilon -1/1000)
+(defvar long-float-epsilon 1/10000)
 
+(defconstant most-negative-short-float most-negative-fixnum)
+(defconstant most-negative-single-float most-negative-fixnum)
+(defconstant most-negative-long-float most-negative-fixnum)
+(defconstant most-negative-double-float most-negative-fixnum)
+
+(defconstant least-positive-short-float 1/100000)
+(defconstant least-positive-single-float 1/100000)
+(defconstant least-positive-double-float 1/100000)
+(defconstant least-positive-long-float 1/100000)
+
+(defconstant least-negative-short-float -1/100000)
+(defconstant least-negative-single-float -1/100000)
+(defconstant least-negative-double-float -1/100000)
+(defconstant least-negative-long-float -1/100000)
 
 (defconstant call-arguments-limit 512)
 (defconstant lambda-parameters-limit 512) ; ?
 
 (defvar *print-pprint-dispatch* nil)
 
-(declaim (special *build-number*))
+(defvar *build-number*) ; set at bootup
+
+(defvar *features* '(:movitz))
+(defvar *modules* nil)
+
+(defvar *compile-file-pathname* nil)
+(defvar *compile-file-truename* nil)
+(defvar *compile-print* nil)
+(defvar *compile-verbose* nil)
+(defvar *load-print* nil)
+(defvar *load-verbose* nil)
+(defvar *load-truename* nil)
+(defvar *default-pathname-defaults* #p"")
